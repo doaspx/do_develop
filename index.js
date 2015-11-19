4eb9cdd6975cacbfaf027ecce66bde1947dd1c2a
= <do_web@163.com>
2015-11-19 13:04:09 +0800

    add project

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * ¿œ√® created at 2014-09-05 18:13
+ *
+ * Copyright (c) 2015 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+exports.Toshihiko = require("./lib/toshihiko");
+exports.Type = require("./lib/fieldType");
+exports.Memcached = require("toshihiko-memcached");
